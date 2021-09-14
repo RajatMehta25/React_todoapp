cccae8f8371ab7ab336b06d7fea456b1e2221bc1
RajatMehta <rjt25881@gmail.com>
2021-09-14 16:01:41 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import react, { useState } from 'react';
+import reactDom from 'react-dom';
+
+function App() {
+
+const [item,setinput]=useState();
+const setitems =(event)=>{
+  setinput(event.target.value);
+}
+
+const [updateitem,arrayitems]=useState([]);
+const listofitems =()=>{
+  if ([item]==""){
+ alert("Enter a Valid Task");
+}
+else{
+  arrayitems((olditems)=>{
+    return [...olditems,item];
+  });
+  setinput("");
+}
+};
+
+  return (
+    <>
+      <div className="container">
+<div className="heading">TO DO LIST</div>
+<div className="mobile"><input type="text" placeholder="ADD A TASK" className="inp" onChange={setitems} value={item}/><button className="btn" onClick={listofitems}>+</button></div>
+<div>
+  <ol>
+    
+    {updateitem.map((itemval)=>{
+      return <li>{itemval}</li>;
+    })}
+   
+  </ol>
+</div>
+      </div>
+      
+      
+    </>
+  );
+}
+
+export default App;
